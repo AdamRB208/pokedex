1967ff032fc0cbdc6d252bf55c25f186bcfa4efa
AdamRB208 <adamrborup@gmail.com>
2025-03-05 15:18:29 -0700

    Pokemon in AppState

--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,11 @@
+import { Pokemon } from './models/Pokemon.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-
+  /**@type {Pokemon[]}*/
+  Pokemon = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
